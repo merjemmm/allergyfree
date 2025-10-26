53ecf2a82056dca579851313e9286cc91b0b7ee8
Merjem Memic <merjem.m47@gmail.com>
2025-10-26 12:20:54 -0400

    trying to serve app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AuthPage from "./pages/Login";
+
+function App() {
+    console.log("inside of App.jsx");
+
+  return (<div>
+    <h1> HELLO !</h1>
+  </div>);
+  // <AuthPage />;
+}
+
+export default App;
