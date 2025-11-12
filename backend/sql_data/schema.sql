d638719f2990810c2211ac1b467c66e5b42c2644
Merjem Memic <merjem.m47@gmail.com>
2025-11-11 19:37:57 -0500

    add to db

--- backend/sql_data/schema.sql
+++ backend/sql_data/schema.sql
@@ -1,5 +1,6 @@
 PRAGMA foreign_keys = ON;
 
+-- tracks all users
 CREATE TABLE users(
   -- userid INTEGER PRIMARY KEY AUTOINCREMENT,
   username VARCHAR(20) PRIMARY KEY,
@@ -9,6 +10,7 @@ CREATE TABLE users(
   created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+-- tracks restaurants they have gone to
 CREATE TABLE restaurants(
   restid INTEGER PRIMARY KEY AUTOINCREMENT,
   location VARCHAR(64) NOT NULL,
@@ -19,6 +21,7 @@ CREATE TABLE restaurants(
   FOREIGN KEY(adder) REFERENCES users(username) ON DELETE CASCADE
 );
 
+-- tracks symptoms they have had
 CREATE TABLE symptoms(
   dateid INTEGER PRIMARY KEY AUTOINCREMENT,
   adder VARCHAR(20) NOT NULL,
@@ -27,17 +30,28 @@ CREATE TABLE symptoms(
   year INTEGER NOT NULL,
   type VARCHAR(20) NOT NULL,
   notes VARCHAR(200) NOT NULL,
+  symptom  VARCHAR(20) NOT NULL,
   FOREIGN KEY(adder) REFERENCES users(username) ON DELETE CASCADE
+  FOREIGN KEY(symptom) REFERENCES symptomCategories(symptom) ON DELETE CASCADE
 );
 
--- CREATE TABLE journal(
---   symptomid INTEGER PRIMARY KEY AUTOINCREMENT,
---   adder VARCHAR(20) NOT NULL,
---   day INTEGER NOT NULL,
---   month INTEGER NOT NULL,
---   year INTEGER NOT NULL,
---   meal VARCHAR(20) NOT NULL,
---   name VARCHAR(20) NOT NULL,
+-- categories for user symptoms
+-- TODO, need some db or otherwise that says what symptoms a user has chosen
+CREATE TABLE symptomCategories(
+  symptomid INTEGER PRIMARY KEY AUTOINCREMENT,
+  symptom VARCHAR(40) NOT NULL,
+  type  VARCHAR(40) NOT NULL,
+);
 
---   FOREIGN KEY(adder) REFERENCES users(username) ON DELETE CASCADE
--- );
+-- tracks foods they have eaten
+CREATE TABLE journal(
+  symptomid INTEGER PRIMARY KEY AUTOINCREMENT,
+  adder VARCHAR(20) NOT NULL,
+  day INTEGER NOT NULL,
+  month INTEGER NOT NULL,
+  year INTEGER NOT NULL,
+  meal VARCHAR(20) NOT NULL,
+  name VARCHAR(20) NOT NULL,
+
+  FOREIGN KEY(adder) REFERENCES users(username) ON DELETE CASCADE
+);
